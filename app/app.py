19ba918eabd3555b72cf3c916f232a60152ed663
MeetLovesEverything <139140131+MeetLovesEverything@users.noreply.github.com>
2025-05-14 22:30:23 +0530

    app.py fix

--- app/app.py
+++ app/app.py
@@ -43,4 +43,5 @@ async def predict_language(audio_file: UploadFile = File(...)):
         return JSONResponse(content={"error": str(e)}, status_code=500)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))  # Render sets PORT env var
+    uvicorn.run("app:app", host="0.0.0.0", port=port, reload=True)
